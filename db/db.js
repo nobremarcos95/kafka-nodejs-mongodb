cd62a144738fea81c642b515f49a8c2079e44347
Marcos Nobre <nobremarcos95@gmail.com>
2022-05-30 07:18:43 -0300

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const uri = "mongodb+srv://gcloud07:ssc0158g072022@gcloud07.kk7msqf.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 client.connect(err => {
